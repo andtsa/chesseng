3556b1f6aa050e9b81bc265426fed32e608b0881
Andreas Tsatsanis <code@andreats.com>
2024-11-13 14:27:19 +0700

    benchmarking

---
+++ src/benches/quick_search.rs
@@ -0,0 +1,40 @@
+use criterion::black_box;
+use criterion::criterion_group;
+use criterion::criterion_main;
+use criterion::Criterion;
+use sandy_engine::search::negamax::ng_test;
+use sandy_engine::setup::depth::Depth;
+use sandy_engine::setup::values::Value;
+use sandy_engine::util::bench_positions;
+use sandy_engine::opts::Opts;
+
+fn search_benches(c: &mut Criterion) {
+    let positions = bench_positions();
+    let samples = [0, 5, 29];
+
+    let mut group = c.benchmark_group("search_bench");
+
+    for p_idx in samples {
+        let startpos = positions[p_idx];
+        group.bench_function(format!("ngm_pos_{}_depth_6", p_idx), |b| {
+            b.iter(|| {
+                ng_test(
+                    black_box(startpos),
+                    black_box(Depth(6)),
+                    black_box(Value::MIN),
+                    black_box(Value::MAX),
+                    Opts::bench(),
+                )
+            })
+        });
+    }
+
+    group.finish();
+}
+
+criterion_group! {
+     name = benches;
+     config = Criterion::default();
+     targets = search_benches
+ }
+criterion_main!(benches);
