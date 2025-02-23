a9bec8a602a2270ac24ad264d12c1362e17eb04b
Andreas Tsatsanis <code@andreats.com>
2025-02-23 15:23:01 +0100

    organise integration tests

--- tests/1m.rs
+++ tests/1m.rs
@@ -1,10 +1,10 @@
-//! How deep can we reach in 10 seconds?
+//! How deep can we reach in 1.5 minute
 use crate::shared::test_uci;
 
 /// get testing functions
 mod shared;
 
-/// run the engine for 10 seconds
+/// run the engine for 90 seconds
 #[test]
 fn main() {
     let sequence = [
@@ -14,7 +14,7 @@ fn main() {
         "setoption name threads value 8",
         "isready",
         "position startpos",
-        "go movetime 10000",
+        "go movetime 90000",
     ];
 
     test_uci(&sequence);
